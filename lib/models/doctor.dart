fd136ffc1878b583ae99f730b58126ea53057e01
vivekbargude <vivekbargude1206@gmail.com>
2024-03-18 20:33:56 +0530

    vivek

--- lib/models/doctor.dart
+++ lib/models/doctor.dart
@@ -1,15 +1,36 @@
 class Doctor {
-  final String imgUrl;
-  final String name;
-  final String position;
-  final double rating;
-  final double distance;
+  String? id;
+  String? doctId;
+  String? doctName;
+  String? doctImage;
+  String? doctCategory;
+  String? doctDesc;
 
-  Doctor({
-    required this.imgUrl,
-    required this.name,
-    required this.position,
-    required this.rating,
-    required this.distance,
-  });
+  Doctor(
+      {this.id,
+      this.doctId,
+      this.doctName,
+      this.doctImage,
+      this.doctCategory,
+      this.doctDesc});
+
+  Doctor.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    doctId = json['doctId'];
+    doctName = json['doctName'];
+    doctImage = json['doctImage'];
+    doctCategory = json['doctCategory'];
+    doctDesc = json['doctDesc'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['doctId'] = this.doctId;
+    data['doctName'] = this.doctName;
+    data['doctImage'] = this.doctImage;
+    data['doctCategory'] = this.doctCategory;
+    data['doctDesc'] = this.doctDesc;
+    return data;
+  }
 }
